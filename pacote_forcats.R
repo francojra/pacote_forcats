cefc091eff9081e22ed56e05e57953ecff17f4af
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-05 23:51:23 -0300

    Add files via upload

--- pacote_forcats.R
+++ pacote_forcats.R
@@ -18,3 +18,28 @@
 # - fct_relevel(): Alterar a ordem de um fator à mão.
 # - fct_lump(): Colapso dos valores menos/mais frequentes de um factor em "outro". 
 
+# Instalação do pacote ---------------------------------------------------------------------------------------------------------------------
+
+### O caminho mais fácil para instalar o pacote forcats é através da 
+### instalação do tidyverse que apresenta um conjunto de pacotes necessários
+### para manipulação de dados.
+
+install.packages("tidyverse")
+
+### Alternativamente, você pode apenas instalar o forcats.
+
+install.packages("forcats")
+
+### Ou através da versão disponível no GitHub
+
+# install.packages("devtools")
+devtools::install_github("tidyverse/forcats")
+
+# Cheatsheet -------------------------------------------------------------------------------------------------------------------------------
+
+### Você pode acessar o folheto com todas as funções do pacote forcats
+### e com exemplos de aplicações.
+
+# Na aba do R clique em 'Help' > Cheat sheets > Browse Cheat sheets
+
+### Após isso, busque o cheat sheet relacioando ao pacote forcats
