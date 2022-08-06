7ee7e430fbcd2458894b3598d4a18a45e532d247
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-05 23:39:41 -0300

    Add files via upload

---
+++ pacote_forcats.R
@@ -0,0 +1,20 @@
+
+# Pacote forcats ---------------------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 05/08/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://forcats.tidyverse.org/ -----------------------------------------------------------------------------------------------
+
+# Introdução -------------------------------------------------------------------------------------------------------------------------------
+
+### R usa fatores para manusear variáveis categóricas, variáveis que tem
+### um fixado e conhecido conjunto de possíveis valores. Fatores também
+### são úteis para reordenar fatores de caracteres para melhorar visualização.
+### O objetivo do pacote forcats é promover um adequado conjunto de 
+### ferramentas que resolve problemas comuns com fatores, incluindo
+### mudar a ordem dos níveis dos fatores. Alguns exemplos incluem:
+
+# - fct_reorder(): Reordenar um fator por uma outra variável.Reordering a factor by another variable.
+# - fct_infreq(): Reordenar um fator pela frequência dos valores. Reordering a factor by the frequency of values.
+# - fct_relevel(): Alterar a ordem de um fator à mão.
+# - fct_lump(): Colapso dos valores menos/mais frequentes de um factor em "outro". 
+
